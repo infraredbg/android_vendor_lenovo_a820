c8f2e75b88a5920cc02d0e7954e0cb3234d64e71
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2017-05-28 20:31:59 +0300

    Update blobs

--- a820-vendor-blobs.mk
+++ a820-vendor-blobs.mk
@@ -16,6 +16,7 @@
 
 PRODUCT_COPY_FILES += \
  vendor/lenovo/a820/proprietary/etc/agps_profiles_conf.xml:system/etc/agps_profiles_conf.xml \
+ vendor/lenovo/a820/proprietary/etc/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml \
  vendor/lenovo/a820/proprietary/etc/firmware/mt6628_patch_e2_1_hdr.bin:system/etc/firmware/mt6628_patch_e2_1_hdr.bin \
  vendor/lenovo/a820/proprietary/etc/firmware/WIFI_RAM_CODE_E6:system/etc/firmware/WIFI_RAM_CODE_E6 \
  vendor/lenovo/a820/proprietary/etc/firmware/mt6628/mt6628_fm_v2_coeff.bin:system/etc/firmware/mt6628/mt6628_fm_v2_coeff.bin \
@@ -47,27 +48,21 @@ PRODUCT_COPY_FILES += \
  vendor/lenovo/a820/proprietary/bin/drvbd:system/bin/drvbd \
  vendor/lenovo/a820/proprietary/bin/thermal:system/bin/thermal \
  vendor/lenovo/a820/proprietary/bin/mtk_agpsd:system/bin/mtk_agpsd \
- vendor/lenovo/a820/proprietary/bin/bmm050d:system/bin/bmm050d \
  vendor/lenovo/a820/proprietary/bin/thermal_manager:system/bin/thermal_manager \
  vendor/lenovo/a820/proprietary/bin/nvram_daemon:system/bin/nvram_daemon \
  vendor/lenovo/a820/proprietary/bin/6620_launcher:system/bin/6620_launcher \
  vendor/lenovo/a820/proprietary/bin/6620_wmt_concurrency:system/bin/6620_wmt_concurrency \
  vendor/lenovo/a820/proprietary/bin/6620_wmt_lpbk:system/bin/6620_wmt_lpbk \
- vendor/lenovo/a820/proprietary/bin/akmd8975:system/bin/akmd8975 \
  vendor/lenovo/a820/proprietary/bin/muxreport:system/bin/muxreport \
  vendor/lenovo/a820/proprietary/bin/permission_check:system/bin/permission_check \
- vendor/lenovo/a820/proprietary/bin/ami304d:system/bin/ami304d \
  vendor/lenovo/a820/proprietary/bin/ccci_fsd:system/bin/ccci_fsd \
  vendor/lenovo/a820/proprietary/bin/wmt_loader:system/bin/wmt_loader \
  vendor/lenovo/a820/proprietary/bin/rild:system/bin/rild \
  vendor/lenovo/a820/proprietary/bin/orientationd:system/bin/orientationd \
  vendor/lenovo/a820/proprietary/bin/pppd:system/bin/pppd \
- vendor/lenovo/a820/proprietary/bin/akmd8963:system/bin/akmd8963 \
  vendor/lenovo/a820/proprietary/bin/wlan_loader:system/bin/wlan_loader \
  vendor/lenovo/a820/proprietary/bin/rildmd2:system/bin/rildmd2 \
  vendor/lenovo/a820/proprietary/bin/ccci_mdinit:system/bin/ccci_mdinit \
- vendor/lenovo/a820/proprietary/bin/msensord:system/bin/msensord \
- vendor/lenovo/a820/proprietary/bin/memsicd:system/bin/memsicd \
  vendor/lenovo/a820/proprietary/bin/nvram_agent_binder:system/bin/nvram_agent_binder \
  vendor/lenovo/a820/proprietary/bin/pppd_dt:system/bin/pppd_dt \
  vendor/lenovo/a820/proprietary/bin/geomagneticd:system/bin/geomagneticd \
@@ -154,6 +149,7 @@ PRODUCT_COPY_FILES += \
  vendor/lenovo/a820/proprietary/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin \
  vendor/lenovo/a820/proprietary/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin \
  vendor/lenovo/a820/proprietary/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin \
+ vendor/lenovo/a820/proprietary/vendor/lib/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
  vendor/lenovo/a820/proprietary/vendor/lib/libsrv_init.so:system/vendor/lib/libsrv_init.so \
  vendor/lenovo/a820/proprietary/vendor/lib/libsrv_um.so:system/vendor/lib/libsrv_um.so \
  vendor/lenovo/a820/proprietary/vendor/lib/libpvr2d.so:system/vendor/lib/libpvr2d.so \
@@ -161,7 +157,6 @@ PRODUCT_COPY_FILES += \
  vendor/lenovo/a820/proprietary/vendor/lib/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
  vendor/lenovo/a820/proprietary/vendor/lib/libIMGegl.so:system/vendor/lib/libIMGegl.so \
  vendor/lenovo/a820/proprietary/vendor/lib/libusc.so:system/vendor/lib/libusc.so \
- vendor/lenovo/a820/proprietary/vendor/lib/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
  vendor/lenovo/a820/proprietary/vendor/lib/egl/libGLESv1_CM_mtk.so:system/vendor/lib/egl/libGLESv1_CM_mtk.so \
  vendor/lenovo/a820/proprietary/vendor/lib/egl/libEGL_mtk.so:system/vendor/lib/egl/libEGL_mtk.so \
  vendor/lenovo/a820/proprietary/vendor/lib/egl/libGLESv2_mtk.so:system/vendor/lib/egl/libGLESv2_mtk.so \
@@ -169,5 +164,4 @@ PRODUCT_COPY_FILES += \
  vendor/lenovo/a820/proprietary/vendor/lib/libBLPP.so:system/vendor/lib/libBLPP.so \
  vendor/lenovo/a820/proprietary/vendor/lib/hw/gralloc.mt6589.so:system/vendor/lib/hw/gralloc.mt6589.so \
  vendor/lenovo/a820/proprietary/lib/libaudio.primary.default.so:system/lib/libaudio.primary.default.so \
- vendor/lenovo/a820/proprietary/lib/hw/audio_policy.default.so:system/lib/hw/audio_policy.default_kk.so \
- vendor/lenovo/a820/proprietary/lib/libcorkscrew.so:system/lib/libcorkscrew.so
+ vendor/lenovo/a820/proprietary/lib/hw/audio_policy.default.so:system/lib/hw/audio_policy.default_kk.so
